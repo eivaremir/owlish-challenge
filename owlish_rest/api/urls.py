a735e8eb4a1c05958f09e55c401f8535e2a8feaa
Eivar Morales <eivaremir@gmail.com>
2021-08-07 21:13:57 -0500

    added hello test view

---
+++ owlish_rest/api/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('hello/',views.HelloAPIView.as_view())
+]
--- owlish_rest/api/views.py
+++ owlish_rest/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloAPIView(APIView):
+    """ TEST API VIEW """
+
+    def get(self,request,format=None):
+        an_apiview = [
+            'test1','test2',2123
+        ]
+
+        return Response({
+            'message':'ok',
+            'an_apiview':an_apiview
+        })
